a9d3b8a1ded0b061e1b071e20f9c6a572c8e0fc9
ArtemisGia <asgiannakopoulou@gmail.com>
2023-09-22 22:25:46 -0400

    create PhY class

--- chat.py
+++ chat.py
@@ -3,17 +3,34 @@ import openai
 
 openai.my_api_key = os.getenv('OPENAI_API_KEY')
 
-messages = [ {"role": "system", "content": 
+class PhY:
+    '''
+    PhY is a intelligent assistant.
+    
+    init: message (str)
+    get_reply: return reply (str)
+    
+    '''
+    def __init__(self, message):
+        self.message = message
+        self.messages = [ {"role": "system", "content": 
               "You are a intelligent assistant."} ]
-while True:
+        self.messages.append(
+            {"role": "user", "content": self.message},)
+        self.chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=self.messages)
+        self.reply = self.chat.choices[0].message.content    
+        self.messages.append({"role": "assistant", "content": self.reply})
+
+    def get_reply(self):
+        return self.reply
+
+if __name__ == '__main__':
     message = input("User : ")
     if message:
-        messages.append(
-            {"role": "user", "content": message},
-        )
-        chat = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=messages
-        )
-    reply = chat.choices[0].message.content
-    print(f"ChatGPT: {reply}")
-    messages.append({"role": "assistant", "content": reply})
+        chat = PhY(message)
+        reply = chat.get_reply()
+        print(f"ChatGPT: {reply}")
+        
+        
+   
